16271ded9ffbcaf5fa5692fe6a18f363c6478b4e
gabriel1049 <gamerfk81@gmail.com>
2023-10-17 22:17:00 -0300

    alteraçõesscript

--- js/scriptmodels.js
+++ js/scriptmodels.js
@@ -1,44 +1,44 @@
-    // Obtém elementos do DOM
-    const continentSelect = document.getElementById("continent");
-    const brandSelect = document.getElementById("brand");
-    const searchButton = document.getElementById("searchButton");
 
-    // URL base
-    const baseUrl = "https://mystore.mysimtravel.com/";
-
-    // Define o redirecionamento com base nas seleções
-    searchButton.addEventListener("click", function() {
-      const selectedContinent = continentSelect.value;
-      const selectedBrand = brandSelect.value;
-
-      if (selectedContinent === "europa" && selectedBrand === "apple") {
-        // Redireciona para a URL específica
-        window.location.href = baseUrl + "Especial5GB";
-      } else {
-        alert("Opções selecionadas não correspondem a um destino válido.");
-      }
-    });
-
-
-
-    /* const urlMappings = {
+    const urlMappings = {
       africa: {
         apple: "https://example.com/africa-apple",
         samsung: "https://example.com/africa-samsung",
         huawei: "https://example.com/africa-huawei",
         xiaomi: "https://example.com/africa-xiaomi",
+        motorola: "https://example.com/oceania-motorola",
+        google: "https://example.com/oceania-google",
       },
       america: {
-        // Mapeie as seleções para URLs correspondentes na América
+        apple: "https://example.com/america-apple",
+        samsung: "https://example.com/america-samsung",
+        huawei: "https://example.com/america-huawei",
+        xiaomi: "https://example.com/america-xiaomi",
+        motorola: "https://example.com/oceania-motorola",
+        google: "https://example.com/oceania-google",
       },
       asia: {
-        // Mapeie as seleções para URLs correspondentes na Ásia
+        apple: "https://example.com/asia-apple",
+        samsung: "https://example.com/asia-samsung",
+        huawei: "https://example.com/asia-huawei",
+        xiaomi: "https://example.com/asia-xiaomi",
+        motorola: "https://example.com/oceania-motorola",
+        google: "https://example.com/oceania-google",
       },
       europa: {
-        // Mapeie as seleções para URLs correspondentes na Europa
+        apple: "https://example.com/europa-apple",
+        samsung: "https://example.com/europa-samsung",
+        huawei: "https://example.com/europa-huawei",
+        xiaomi: "https://example.com/europa-xiaomi",
+        motorola: "https://example.com/oceania-motorola",
+        google: "https://example.com/oceania-google",
       },
       oceania: {
-        // Mapeie as seleções para URLs correspondentes na Oceania
+        apple: "https://example.com/oceania-apple",
+        samsung: "https://example.com/oceania-samsung",
+        huawei: "https://example.com/oceania-huawei",
+        xiaomi: "https://example.com/oceania-xiaomi",
+        motorola: "https://example.com/oceania-motorola",
+        google: "https://example.com/oceania-google",
       }
     };
 
@@ -58,4 +58,4 @@
       } else {
         alert("Opções selecionadas não correspondem a um destino válido.");
       }
-    }); */
+    });
