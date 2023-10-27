b3960ac2aee3b0ae43b98f92a2f3750554efa7c8
gabriel1049 <gamerfk81@gmail.com>
2023-10-27 15:53:00 -0300

    h2

--- js/scriptcards.js
+++ js/scriptcards.js
@@ -60,9 +60,9 @@ function listOptions(id) {
     textLayout.show();
     ulListUsa.show();
     opcao.hide();
-    $([document.documentElement, document.body]).animate({
-      scrollTop: $("#section-card-usa").offset().top -0
-     }, 500);
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
     padrao();
   });
 
@@ -72,12 +72,10 @@ function padrao() {
   btnVoltar.click(() => {
     sectionCardUsa.hide();
     cardGlobal.show();
-    $([document.documentElement, document.body]).animate({
-      scrollTop: $("#section-card-usa").offset().top -100
-     }, 500);
+$('html, body').animate({
+  scrollTop: 0
+}, 1000);
   });
 
-  $([document.documentElement, document.body]).animate({
-    scrollTop: $("#section-card-usa").offset().top -100
-   }, 500);
+
 }
